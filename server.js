bb47f2d48f2498c415520e3414fe459a1a0bd170
Maitulya7 <vaghelamaitulya85@gmail.com>
2024-03-20 18:19:47 +0530

    CROS Added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorhandler");
 const connectDb = require("./config/dbConnection");
+const cors = require("cors"); 
 const dotenv = require("dotenv").config();
 
 connectDb();
@@ -9,6 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 5000 ;
 
 //middleware 
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these methods
+    allowedHeaders: ['Content-Type', 'Authorization'] // Allow these headers
+  }));
 app.use(express.json());
 app.use("/api/contacts" , require("./routes/contactRoutes"))
 app.use("/api/users" , require("./routes/userRoutes"))
